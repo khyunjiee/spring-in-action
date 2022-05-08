ebd3d8b9a6a98eb1073207683a2a1d48abc3baf5
khyunjiee <khyunjiee@gmail.com>
2022-05-08 16:45:25 +0900

    update: 타코 디자인 form submit 컨트롤러 함수 생성

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Ingredient;
 import tacos.Taco;
@@ -41,6 +42,14 @@ public class DesignTacoController {
         model.addAttribute("taco", new Taco());
 
         return "design";
+    }
+
+    @PostMapping
+    public String processDesign(Taco design) {
+        // 이 지점에서 타코 디자인(선택된 식자재 내역)을 저장한다
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
     }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type) {
